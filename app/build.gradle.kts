35e9866c4f1dc5d9ef8bb25972ab56539a0f954f
xxx <enpunk90@gmail.com>
2026-01-14 00:39:39 +0900

    Update dependencies versions with kotlin
    ktor, okhttp etc compiled kotlin 2.2.x

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 
 plugins {
@@ -48,9 +49,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
+
     buildFeatures {
         compose = true
     }
