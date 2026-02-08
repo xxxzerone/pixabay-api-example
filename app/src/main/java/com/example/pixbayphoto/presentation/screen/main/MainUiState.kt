b73123c352a78171e9590943fba8b92e9c0a9903
xxx <enpunk90@gmail.com>
2026-02-08 15:43:11 +0900

    Add MainUiState and MainViewModel

---
+++ app/src/main/java/com/example/pixbayphoto/presentation/screen/main/MainUiState.kt
@@ -0,0 +1,9 @@
+package com.example.pixbayphoto.presentation.screen.main
+
+import com.example.pixbayphoto.domain.model.Item
+
+data class MainUiState(
+    val items: List<Item> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
---
+++ app/src/main/java/com/example/pixbayphoto/presentation/screen/main/MainViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pixbayphoto.presentation.screen.main
+
+import androidx.lifecycle.ViewModel
+import com.example.pixbayphoto.domain.usecase.GetItemsSortedByIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getItemsSortedByIdUseCase: GetItemsSortedByIdUseCase
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState = _uiState.asStateFlow();
+
+}
